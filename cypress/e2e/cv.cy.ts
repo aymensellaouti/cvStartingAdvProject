a99e4422ab5c4f0f0e534f2152685ea185257592
aymen2782 <aymen.sellaouti@gmail.com>
2023-02-15 17:31:28 +0100

    cypress tests examples

---
+++ cypress/e2e/cv.cy.ts
@@ -0,0 +1,21 @@
+import { API } from './../../src/config/api.config';
+
+describe('Cv List', () => {
+  it('Visits the list cv page', () => {
+    cy.intercept(API.cv, { fixture: 'cvs.json' });
+
+    cy.visit('/cv');
+    cy.contains('cvStartingAdvProject');
+    /* list-cv */
+
+    const listJuniors = cy.get('[data-testid=list-cv]').first();
+    listJuniors.should('have.length', 1);
+    listJuniors.first().contains('Skander');
+    listJuniors.should('be.visible');
+    const listSeniors = cy.get('[data-testid=list-cv]').last();
+    listSeniors.should('have.length', 1);
+    listSeniors.should('not.be.visible');
+    listSeniors.first().contains('Aymen');
+    cy.get('[data-testid=cv-card]').should('not.exist');
+  });
+});
