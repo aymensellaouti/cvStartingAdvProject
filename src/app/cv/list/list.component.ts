83c5ee1bd8e543e9d75868e0e957a0e1d964dc00
aymen2782 <aymen.sellaouti@gmail.com>
2023-03-21 11:20:09 +0100

    trackBy

--- src/app/cv/list/list.component.ts
+++ src/app/cv/list/list.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Cv } from '../model/cv';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Cv } from "../model/cv";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
 })
 export class ListComponent {
   @Input() cvs: Cv[] | null = [];
+  trackByFn(iteration: number, cv: Cv) {
+    console.log(cv);
+    console.log(iteration);
+
+    return cv.id;
+  }
   /*   @Output() forwardCv = new EventEmitter<Cv>(); */
 }
