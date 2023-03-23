806b5639d1f546b5c8bd746c80d1ca18d4b1c969
aymen2782 <aymen.sellaouti@gmail.com>
2023-03-23 11:58:28 +0100

    subject behaviourSubject ..

---
+++ src/app/auth/model/user.model.ts
@@ -0,0 +1,3 @@
+export class User {
+  constructor(private id = 0, private email = "") {}
+}
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,25 +1,47 @@
-import { Injectable } from '@angular/core';
-import { CredentialsDto } from '../dto/credentials.dto';
-import { LoginResponseDto } from '../dto/login-response.dto';
-import { HttpClient } from '@angular/common/http';
-import { API } from '../../../config/api.config';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { CredentialsDto } from "../dto/credentials.dto";
+import { LoginResponseDto } from "../dto/login-response.dto";
+import { HttpClient } from "@angular/common/http";
+import { API } from "../../../config/api.config";
+import { BehaviorSubject, Observable, Subject, map, tap } from "rxjs";
+import { User } from "../model/user.model";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
-
+  private userSubject = new BehaviorSubject<User | null>(null);
+  user$ = this.userSubject.asObservable();
+  isLoggedIn$!: Observable<boolean>;
+  isLoggedOut$!: Observable<boolean>;
+  constructor(private http: HttpClient) {
+    this.isLoggedIn$ = this.user$.pipe(
+      map((user) => !!user && this.isAuthenticated())
+    );
+    this.isLoggedOut$ = this.user$.pipe(map((user) => !user));
+    const user = JSON.parse(localStorage.getItem("user") ?? "");
+    if (user) {
+      this.userSubject.next(user);
+    }
+  }
   login(credentials: CredentialsDto): Observable<LoginResponseDto> {
-    return this.http.post<LoginResponseDto>(API.login, credentials);
+    return this.http.post<LoginResponseDto>(API.login, credentials).pipe(
+      tap((response: LoginResponseDto) => {
+        const user = new User(response.userId, credentials.email);
+        localStorage.setItem("token", response.id);
+        localStorage.setItem("user", JSON.stringify(user));
+        this.userSubject.next(user);
+      })
+    );
   }
 
   isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
+    return !!localStorage.getItem("token");
   }
 
   logout() {
-    localStorage.removeItem('token');
+    this.userSubject.next(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
   }
 }
